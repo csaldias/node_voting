1fbb813125d7ccde9dbf9c3e1296ac14776e3a4c
Camilo Saldias <camilo.saldias@gmail.com>
2014-05-08 00:14:56 -0400

    Borrados recursos innecesarios en el frontend.

--- html/old/index_.php
+++
@@ -1,9 +0,0 @@
-<DOCTYPE html>
-<html>
-	<head>
-		<title>Conteo de Votos, Elecciones CEEINF 2013</title>
-	</head>
-	<body style="overflow:hidden;">
-		<p align="center"><img src="bg.png" alt="Elecciones CEEINF 2013" style="margin:1%;" /></p>
-	</body>
-</html>
--- html/old/update.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-file_put_contents("listam.txt", $_POST['listam']);
-file_put_contents("listaz.txt", $_POST['listaz']);
-file_put_contents("blanco.txt", $_POST['blanco']);
-file_put_contents("null.txt", $_POST['nulo']);
-file_put_contents("info.txt", $_POST['info']);
-
-?>
